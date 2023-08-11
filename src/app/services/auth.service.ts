bea67fda664ec25d39d3a89c7279d49b7304f615
Roni carrascal <ronnicarrascaltejedor@gmail.com>
2023-08-11 11:17:48 -0500

    se creo funcion de alerta

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 
 @Injectable({
@@ -8,7 +9,8 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   constructor(
-    private router:Router
+    private router:Router,
+    private alertCtrl: AlertController,
     ) { }
 
   async register(email:string, password: string): Promise<void>{
@@ -26,6 +28,15 @@ export class AuthService {
 
   async logout() {
 
+  }
+  async alert(message:string){
+    const alert = await this.alertCtrl.create({
+      header: 'Error',
+      message: message,
+      buttons: ['ok']
+    })
+
+    await alert.present();
   }
 
   
