ef6fb727fcb4849e7dfb2eed6c966e25d6a0531d
Roni carrascal <ronnicarrascaltejedor@gmail.com>
2023-08-11 11:41:50 -0500

    se agrego ruta del home y profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
   }
 ];
 @NgModule({
