dd0a32de23f1206c8b32393774189e7e7eb05005
Roni Carrascal <64283528+ronni18@users.noreply.github.com>
2023-08-21 23:37:01 -0500

    se integro firestore

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collectionData, collection, addDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
+  firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  constructor(
+  //  private firestore : Firestore,
+  ) { }
 
   saveTasks(task:any){
+    const tasks = collection(this.firestore, 'tasks')
+    return addDoc(tasks, task)
 
   }
   deleteTask(task:any){
 
   }
+  
 }
