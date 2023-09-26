6b763700628157ab34c42a95c769a2b40cdb7ab2
Roni Carrascal <ronnicarrascaltejedor@gmail.com>
2023-09-25 23:26:59 -0500

    add angular fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyBwmchRuv3EEuwpByj4UigbDHawylvUY-Q",
-    authDomain: "vero-139ce.firebaseapp.com",
-    projectId: "vero-139ce",
-    storageBucket: "vero-139ce.appspot.com",
-    messagingSenderId: "1022534217458",
-    appId: "1:1022534217458:web:c62f0c4f5e1d6c81476a6b",
-    measurementId: "G-DQ3WMFH0CZ"
+    projectId: 'vero-139ce',
+    appId: '1:1022534217458:web:c62f0c4f5e1d6c81476a6b',
+    storageBucket: 'vero-139ce.appspot.com',
+    apiKey: 'AIzaSyBwmchRuv3EEuwpByj4UigbDHawylvUY-Q',
+    authDomain: 'vero-139ce.firebaseapp.com',
+    messagingSenderId: '1022534217458',
+    measurementId: 'G-DQ3WMFH0CZ',
   },
   production: false
 };
